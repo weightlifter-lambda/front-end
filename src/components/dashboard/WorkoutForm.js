242294637769366bf3e4880f48a7328e3d2d19ca
Hiter Harris <hiterharris@comcast.net>
2020-01-07 17:01:34 -0500

    pushing dashboard

--- src/components/dashboard/WorkoutForm.js
+++ src/components/dashboard/WorkoutForm.js
@@ -30,6 +30,14 @@ const WorkoutForm = () => {
 
   return (
     <form onSubmit={submitForm}>
+    <div class="dropdown">
+  <button class="dropbtn">Dropdown</button>
+  <div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
       <label htmlFor="title">Exercise</label>
       <input
         id="exercise"
