0cb9fdd9a3a866524ddd98bb869e7e846bb2b8cc
Hiter Harris <hiterharris@comcast.net>
2020-01-08 13:16:58 -0500

    added greg-hayes changes to dashboard branch

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,8 +1,59 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { register } from "../state/actions";
-
-const SignUpForm2 = props => {
+import styled, { ThemeProvider } from "styled-components";
+const theme = {
+  colors: {
+    yellow: "#FBB338",
+    grey_main: "grey",
+    grey_hilite: "",
+    blue: "#06A8FF"
+  }
+};
+const Container = styled.body`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`;
+const Header = styled.header`
+  display: flex;
+  height: 15%;
+  width: 100%;
+  border: 1px solid red;
+`;
+const Login = styled.div`
+  display: flex;
+  height: 50%;
+  width: 20%;
+  margin: 3em 0 5em 0;
+  padding: 3rem;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid red;
+  border-radius: 10px;
+`;
+const Fields = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+  font-family: "Russo One";
+`;
+const Button = styled.button`
+  font-family: "Russo One";
+  &:hover {
+    background: yellow;
+  }
+`;
+const Footer = styled.footer`
+  display: flex;
+  height: 15%;
+  width: 100%;
+  border: 1px solid blue;
+`;
+const SignUpForm = props => {
   const [user, setUser] = useState({
     firstName: "",
     lastName: "",
@@ -27,39 +78,50 @@ const SignUpForm2 = props => {
     });
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <label>First Name</label>
-      <input
-        type="text"
-        id="firstName"
-        name="firstName"
-        onChange={handleChanges}
-      />
-      <label>Last Name</label>
-      <input
-        type="text"
-        id="lastName"
-        name="lastName"
-        onChange={handleChanges}
-      />
-      <label>Email</label>
-      <input type="email" id="email" name="email" onChange={handleChanges} />
-      <label>Password</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        onChange={handleChanges}
-      />
-      <button> Register </button>
-    </form>
+    <Container>
+      <Header>header</Header>
+      <Login>
+        <form onSubmit={handleSubmit}>
+          <Fields>
+            <label>First Name</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              onChange={handleChanges}
+            />
+            <label>Last Name</label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              onChange={handleChanges}
+            />
+            <label>Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={handleChanges}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={handleChanges}
+            />
+          </Fields>
+          <Button> Register </Button>
+        </form>
+      </Login>
+      <Footer>footer</Footer>
+    </Container>
   );
 };
-
 const mapStateToProps = state => {
   return {
     ...state
   };
 };
-
-export default connect(mapStateToProps, { register })(SignUpForm2);
+export default connect(mapStateToProps, { register })(SignUpForm);
